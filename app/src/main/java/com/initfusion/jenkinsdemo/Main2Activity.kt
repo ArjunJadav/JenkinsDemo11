5da02145231bba2f3a94bd8840752295c5775377
Arjun Jadav <Arjun.jadav@initfusion.com>
2017-12-21 12:29:42 +0530

    Initial commit

---
+++ app/src/main/java/com/initfusion/jenkinsdemo/Main2Activity.kt
@@ -0,0 +1,38 @@
+package com.initfusion.jenkinsdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val btnPrintToast = findViewById<Button>(R.id.btnToast)
+        val btnRunTimeException = findViewById<Button>(R.id.btnRunTimeException)
+
+        btnPrintToast.setOnClickListener {
+            var str = editText.text.toString()
+            if (str.trim().isEmpty()) {
+                str = "Please enter some value to editText and then try"
+            }
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+        }
+
+        btnRunTimeException.setOnClickListener {
+            var str = editText.text.toString()
+            if (str.trim().isEmpty()) {
+                str = "Please enter some value to editText and then try to throw exception"
+            }
+//            throw RuntimeException(str)
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+}
